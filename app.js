feaf91f0732c9d16dd57be07890210cadffc8bea
eblahm <matthew@webs.com>
2015-03-10 13:09:00 -0400

    add a bunch of dependencies that we will eventually need, also add a bit of logging

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 var express = require('express');
 var app = express();
 
+var hbs = require('express-hbs');
+var path = require('path');
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(hbs.express3({extname: '.html'}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
 	res.send('welome to the office alert app');
 });
 
-app.listen(15001);
+var port = 15001;
+app.listen(port);
+console.log('express listening on port', port);
